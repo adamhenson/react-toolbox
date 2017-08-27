0dfcb295c3b3b3be4b0c5ca1573978811d65691a
adamhenson <adamhenson1979@gmail.com>
2017-08-27 19:10:01 -0400

    IconMenu: active prop (#1662).
    (cherry picked from commit 74bd2dc77bd0a09245694be15e479ae6722f11af)

--- components/menu/IconMenu.js
+++ components/menu/IconMenu.js
@@ -62,8 +62,8 @@ const factory = (IconButton, Menu) => {
 
     render () {
       const {
-        active, children, className, icon, iconRipple, menuRipple, // eslint-disable-line
-        onHide, onSelect, onShow, position, selectable, selected, theme, ...other
+        active, children, className, icon, iconRipple, menuRipple, onHide, // eslint-disable-line
+        onSelect, onShow, position, selectable, selected, theme, ...other
       } = this.props;
       return (
         <div {...other} className={classnames(theme.iconMenu, className)}>
